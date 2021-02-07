30e3d89427058604216c1758a1779a190ca33395
shirokuro <46kuro212@gmail.com>
2021-02-07 11:15:47 +0900

    Add feature to name entry

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'dart:collection';
+import 'dart:io';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'dart:convert' as convert;
 import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -28,6 +31,7 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  final HostNameCache _hostNameCache = HostNameCache();
   List<_ClientInfo> _clients;
 
   Future<List<_ClientInfo>> _retrieveClients() async {
@@ -65,6 +69,13 @@ class _MainPageState extends State<MainPage> {
         _clients = value;
       });
     });
+    HostNameCache.readFromFile().then((entries) {
+      setState(() {
+        for (var entry in entries) {
+          _hostNameCache.putEntry(entry);
+        }
+      });
+    });
   }
 
   Future _updateStatus() async {
@@ -78,30 +89,75 @@ class _MainPageState extends State<MainPage> {
     });
   }
 
-  ExpansionPanel _createExpansionPanel(_ClientInfo client) {
-    String leaseStr = "";
+  Future<String> _showHostNameInputDialog(
+      BuildContext context, String currentText) async {
+    final textController = TextEditingController(text: currentText ?? "");
+    final textField = new SimpleDialogOption(
+      child: TextField(
+        controller: textController,
+        decoration: InputDecoration(hintText: "Enter hostname here"),
+      ),
+    );
+    final okButton = FlatButton(
+        onPressed: () {
+          Navigator.pop<String>(context, textController.text);
+        },
+        child: Text("OK"));
+    final cancelButton = FlatButton(
+        onPressed: () {
+          Navigator.pop<String>(context, null);
+        },
+        child: Text("Cancel"));
+    final alertDialog = AlertDialog(
+      title: Text("Change hostname"),
+      content: textField,
+      actions: [cancelButton, okButton],
+    );
+    return await showDialog(
+        context: context, builder: (context) => alertDialog);
+  }
+
+  String _convertClientToPanelBody(_ClientInfo client) {
+    String leaseStr = "DHCP: None\n";
     if (client.lease != null) {
-      leaseStr = "Hostname: ${client.lease.hostName ?? "Unknown"}\n"
-          "IPv4 Address: ${client.lease.ipv4Address}\n"
-          "IPv4 Address Expire: ${client.lease.ipv4AddressExpire}\n";
+      leaseStr = "DHCP:\n"
+          "  Hostname: ${client.lease.hostName ?? "Unknown"}\n"
+          "  Address: ${client.lease.ipv4Address}\n"
+          "  Expire At: ${client.lease.ipv4AddressExpire}\n";
     }
-    String ipv6Str = "IPv6 Addresses: None\n";
+    String ipv6Str = "NDP Entries: None\n";
     if (client.ipv6Addresses.isNotEmpty) {
-      ipv6Str = "IPv6 Addresses:\n  - ${client.ipv6Addresses.join("\n  ")}\n";
+      ipv6Str = "NDP Entries:\n  - ${client.ipv6Addresses.join("\n  - ")}\n";
     }
+    return "Mac: ${client.mac}\n$leaseStr$ipv6Str";
+  }
+
+  ExpansionPanel _createExpansionPanel(_ClientInfo client) {
     return ExpansionPanel(
         headerBuilder: (context, isExpanded) {
-          String title = client.mac;
-          if (client.lease != null && client.lease.hostName != null) {
-            title = client.lease.hostName;
-          }
-          return Text(title);
+          String mac = client.mac;
+          final hostName = _hostNameCache.get(mac);
+          final title =
+              mac + (hostName == null ? "" : " [${hostName.hostName}]");
+          return GestureDetector(
+            child: Text(title),
+            onLongPress: () async {
+              final String newHostname = await _showHostNameInputDialog(
+                  context, hostName == null ? "" : hostName.hostName);
+              if (newHostname == null) {
+                return;
+              }
+              _hostNameCache.put(mac, newHostname);
+              _hostNameCache.writeToFile();
+              setState(() {});
+            },
+          );
         },
         isExpanded: client.expanded ?? false,
         canTapOnHeader: true,
         body: Padding(
           child: Align(
-            child: Text("Mac: ${client.mac}\n$leaseStr$ipv6Str",
+            child: Text(_convertClientToPanelBody(client),
                 textAlign: TextAlign.left),
             alignment: Alignment.topLeft,
           ),
@@ -140,6 +196,68 @@ class _MainPageState extends State<MainPage> {
       body: body,
     );
   }
+}
+
+class HostNameCache {
+  final Map<String, HostNameCacheEntry> _entries = HashMap();
+
+  HostNameCacheEntry get(String mac) {
+    return _entries[mac];
+  }
+
+  void put(String mac, String hostName) {
+    putEntry(HostNameCacheEntry(mac, hostName));
+  }
+
+  void putEntry(HostNameCacheEntry entry) {
+    _entries[entry.mac] = entry;
+  }
+
+  HostNameCacheEntry getOr(String mac, String Function() ifAbsent) {
+    var entry = _entries[mac];
+    if (entry == null) {
+      final generated = ifAbsent();
+      if (generated != null) {
+        entry = HostNameCacheEntry(mac, generated);
+      }
+    }
+    return entry;
+  }
+
+  static Future<File> getFile() async {
+    final dataFolder = await getApplicationDocumentsDirectory();
+    print(dataFolder);
+    final file = File("${dataFolder.path}/hostname_cache.json");
+    print(file);
+    return file;
+  }
+
+  static Future<List<HostNameCacheEntry>> readFromFile() async {
+    final file = await getFile();
+    final Map<String, dynamic> decoded =
+        convert.jsonDecode(await file.readAsString());
+    final List<HostNameCacheEntry> result = List();
+    decoded.forEach((key, value) {
+      result.add(HostNameCacheEntry(key, value));
+    });
+    return result;
+  }
+
+  void writeToFile() async {
+    final file = await getFile();
+    final Map<String, String> serializable = HashMap();
+    for (final value in _entries.values) {
+      serializable[value.mac] = value.hostName;
+    }
+    await file.writeAsString(convert.jsonEncode(serializable));
+  }
+}
+
+class HostNameCacheEntry {
+  final String mac;
+  final String hostName;
+
+  HostNameCacheEntry(this.mac, this.hostName);
 }
 
 class _ClientInfo {
